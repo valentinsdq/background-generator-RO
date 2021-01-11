2939811d06f2b32a85185ccda560c7aeb0bc0c52
valentinsdq <stematevalentindaniel@gmail.com>
2021-01-11 17:00:15 +0100

    adding projectro

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# background-generator-RO
+# background-generator
---
+++ script.js
@@ -0,0 +1,29 @@
+var _ = require('lodash');
+
+var array = [1, 2, 3, 4, 5, 6, 7, 8];
+console.log('answer', _.without(array, 3));
+
+var css = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var color3 = document.querySelector(".color3");
+var body = document.getElementById("gradient");
+
+function setGradient() {
+	body.style.background =
+		"linear-gradient(to right, "
+		+ color1.value
+		+ ", "
+		+ color2.value
+		+ ", "
+		+ color3.value
+		+ ")";
+
+	css.textContent = body.style.background + ";";
+}
+
+color1.addEventListener("input", setGradient);
+
+color2.addEventListener("input", setGradient);
+
+color3.addEventListener("input", setGradient);
